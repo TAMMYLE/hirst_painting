bd1cc37f1db505510c6044a85b5fafabd78e0285
TammyLeDeakin <letram3635@gmail.com>
2024-07-19 14:01:08 +1000

    create straingt_line function that draw a line of 10 dots

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import colorgram
+import turtle
+import random
 
-t_colors = colorgram.extract('image.jpg', 30)
+tim = turtle.Turtle()
+
+
 
 extracted_colors = [(252, 250, 247), (253, 247, 249), (237, 251, 245), (249, 228, 17), (213, 13, 9), (198, 12, 35), 
                     (231, 228, 5), (197, 69, 20), (33, 90, 188), (43, 212, 71), (234, 148, 40), (33, 30, 152),
                     (16, 22, 55), (66, 9, 49), (240, 245, 251), (244, 39, 149), (65, 202, 229), (14, 205, 222), 
                     (63, 21, 10), (224, 19, 111), (229, 165, 8), (15, 154, 22), (245, 58, 16), (98, 75, 9),
-                    (248, 11, 9), (222, 140, 203), (68, 240, 161), (10, 97, 62), (5, 38, 33), (68, 219, 155)]
-
+                    (222, 140, 203), (68, 240, 161), (5, 38, 33), (68, 219, 155)]
+turtle.colormode(255)
 # extracting RGB colors from an image
 """ for i in t_colors:
 
@@ -20,4 +24,34 @@ extracted_colors = [(252, 250, 247), (253, 247, 249), (237, 251, 245), (249, 228
     extracted_colors.append(extracted_color)
 
 print(extracted_colors) """
+
+# main program requirements
+
+""" 1. 10x10 rows of spots 
+2. each spot size 20, space 50 in between """  
+
+def straight_line():
+
+    for space in range(10):
+        tim.dot(20, random.choice(extracted_colors))
+        tim.forward(50)
+
+# start_x, start_y: starting coordinates for the turtle
+# line_height: vertical distance between lines
+# 'y' position is adjusted by adding line_height * i for each iteration to move turtle up
+
+start_x = -250
+start_y = -250
+line_height = 50 
+
+for i in range(10):
+    tim.hideturtle()
+    tim.penup()
+    tim.setpos(start_x, start_y + line_height * i)
+    tim.showturtle()
+    straight_line()
+
+# create new screen that can behave what we need
+screen = turtle.Screen()
+screen.exitonclick() 
 
